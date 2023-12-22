65b3ec96d7303d369e7dcc593859057f0d7d40ee
sahilrawat08 <rawatsahil1508@gmail.com>
2023-12-22 20:19:14 +0530

    first commit

---
+++ 2k24/first.js
@@ -0,0 +1,18 @@
+console.log("first js program");
+fullname = "tony stark";
+let name= "sahil";
+const nam ="rawat";
+console.log(nam);
+
+
+const product={
+
+    title:"ball point piere cardin ",
+    rating:4,
+    deal:5,
+    price:250,
+
+}
+
+
+console.log(product);
